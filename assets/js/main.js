32645c916a1598794cb54bd5431610207e32e9c2
Ian Marshall <ian.js.tutor@gmail.com>
2022-09-11 10:17:13 -0400

    added js style retrieval and modal display

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,6 @@
+const rulesList = document.styleSheets[2];
+const cssRules = [...rulesList.cssRules];
+
 document.querySelector("header button").addEventListener("click", ({currentTarget: target}) => {
     if (target.textContent.startsWith("Pause")) {
         //pause all animations
@@ -9,4 +12,22 @@ document.querySelector("header button").addEventListener("click", ({currentTarge
         target.innerHTML = "Pause All <span>&#10074;&#10074;</span>";
         document.querySelectorAll(".spinner").forEach(s => s.classList.remove("pause"));
     }
+});
+
+document.querySelectorAll("main a").forEach(a => {
+    a.addEventListener("click", e => {
+        e.preventDefault();
+        const className = a.className.replace("spinner ", "");
+        const rules = cssRules.filter(r => r.selectorText?.includes(className) || r.name?.includes(className)).map(r => r.cssText);
+        const aside = document.createElement("aside");
+        aside.innerHTML = `<div>
+            <button>Close <span>&times;</span></button>
+            <span class="spinner ${className}"></span>
+            <p>${rules.join("</p><p>")}</p>
+        </div>`;
+        document.body.append(aside);
+        document.querySelector("aside:last-of-type button").addEventListener("click", e => {
+            aside.outerHTML = "";
+        });
+    });
 });
